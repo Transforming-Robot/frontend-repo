adf852bf4c4c4fa27d66f7fae3c553d3203d5f23
yongjunhyun <dev2diver@gmail.com>
2023-01-06 13:32:13 +0900

    story: PageLayout 컴포넌트를 사용한 메인 페이지 렌더링 스토리 작성

--- src/components/layout/PageLayout/index.stories.tsx
+++ src/components/layout/PageLayout/index.stories.tsx
@@ -1,34 +1,70 @@
 import React from 'react';
+import styled from 'styled-components';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { PageLayout } from '.';
-import { LoadingDots } from '../../components/atoms/LoadingDots';
-
-import styled from 'styled-components';
-
-// PageLayout 크기 맞추기 위한 임시 커버 컴포넌트
-const TempContainer = styled.div`
-  width: 1440px;
-  height: 1024px;
-`;
+import { Title, Button, LoadingDots } from '@/components/atoms';
 
 export default {
   title: 'layouts/PageLayout',
   component: PageLayout,
 } as ComponentMeta<typeof PageLayout>;
 
+const PageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+
+  min-width: 50%;
+  min-height: 50%;
+`;
+
 const Template: ComponentStory<typeof PageLayout> = (args) => (
-  <TempContainer>
-    <PageLayout>{args.children}</PageLayout>
-  </TempContainer>
+  <PageLayout>{args.children}</PageLayout>
 );
 
 export const Default = Template.bind({});
 Default.args = {
-  children: <div>레이아웃 테스트</div>,
+  children: <PageContainer>레이아웃 테스트</PageContainer>,
 };
 
 export const LoadingPage = Template.bind({});
 LoadingPage.args = {
-  children: <LoadingDots />,
+  children: (
+    <PageContainer>
+      <LoadingDots />
+    </PageContainer>
+  ),
+};
+
+const MainPageButtonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+export const MainPageBeforeLogin = Template.bind({});
+MainPageBeforeLogin.args = {
+  children: (
+    <PageContainer>
+      <Title title="ChatterBox" />
+      <MainPageButtonContainer>
+        <Button label="로그인 하러가기" />
+        <Button label="회원가입 하러가기" />
+      </MainPageButtonContainer>
+    </PageContainer>
+  ),
+};
+
+export const MainPageAfterLogin = Template.bind({});
+MainPageAfterLogin.args = {
+  children: (
+    <PageContainer>
+      <Title title="ChatterBox" />
+      <MainPageButtonContainer>
+        <Button label="채팅 하러가기" />
+      </MainPageButtonContainer>
+    </PageContainer>
+  ),
 };
