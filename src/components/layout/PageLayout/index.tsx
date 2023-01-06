d7b6fb03d8e212688053e137de49861b2571feda
yongjunhyun <dev2diver@gmail.com>
2023-01-06 13:31:35 +0900

    style: PageLayout 컴포넌트 뷰포트에 따른 너비 높이 지정

--- src/components/layout/PageLayout/index.tsx
+++ src/components/layout/PageLayout/index.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
-  width: 100%;
-  height: 100%;
+  width: 100vw;
+  height: 100vh;
 
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
 
