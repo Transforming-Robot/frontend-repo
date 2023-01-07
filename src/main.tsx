4b93ccf93082979e5229d8872c31e58e04314acd
yongjunhyun <dev2diver@gmail.com>
2023-01-07 17:40:44 +0900

    style: main.tsx 스타일 변경

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from '@/App';
 import './index.css';
+
 import { worker } from '@/api/mock/browser';
 
 if (process.env.NODE_ENV === 'development') {
