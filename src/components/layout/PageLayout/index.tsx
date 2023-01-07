610e0a5cbed38564f2b76ed694155b41b9b83a8b
yongjunhyun <dev2diver@gmail.com>
2023-01-07 17:41:29 +0900

    style: PageLayout 컴포넌트 너비, 높이 제거

--- src/components/layout/PageLayout/index.tsx
+++ src/components/layout/PageLayout/index.tsx
@@ -2,9 +2,6 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
-  width: 100vw;
-  height: 100vh;
-
   display: flex;
   flex-direction: column;
   justify-content: center;
