a02e25541e1b6ca19f24e12a562117882fc90374
yongjunhyun <dev2diver@gmail.com>
2023-01-07 16:38:18 +0900

    feat: RegisterForm 컴포넌트에 useUserRegisterStore로 이벤트 핸들러 연결

--- src/components/blocks/RegisterForm/index.tsx
+++ src/components/blocks/RegisterForm/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Title, FormInput, Button } from '@/components/atoms';
+import { useUserRegisterStore } from '@/stores';
 
 const Container = styled.div`
   display: flex;
@@ -18,17 +19,43 @@ const FormContainer = styled.div`
 `;
 
 export function RegisterForm() {
+  const [user, setUser] = useUserRegisterStore((state) => [
+    state.user,
+    state.setUser,
+  ]);
+
   return (
     <Container>
       <Title title="ChatterBox" />
       <FormContainer>
-        <FormInput label="이메일" placeHolder="이메일을 입력해주세요" />
-        <FormInput label="비밀번호" placeHolder="비밀번호를 입력해주세요" />
         <FormInput
+          id="email"
+          label="이메일"
+          placeHolder="이메일을 입력해주세요"
+          value={user.email}
+          onChange={(event) => setUser(event.target.value, 'email')}
+        />
+        <FormInput
+          id="password"
+          label="비밀번호"
+          placeHolder="비밀번호를 입력해주세요"
+          value={user.password}
+          onChange={(event) => setUser(event.target.value, 'password')}
+        />
+        <FormInput
+          id="passwordCheck"
           label="비밀번호 중복확인"
           placeHolder="비밀번호를 입력해주세요"
+          value={user.passwordCheck}
+          onChange={(event) => setUser(event.target.value, 'passwordCheck')}
         />
-        <FormInput label="닉네임" placeHolder="닉네임을 입력해주세요" />
+        <FormInput
+          id="nickName"
+          label="닉네임"
+          placeHolder="닉네임을 입력해주세요"
+          value={user.nickName}
+          onChange={(event) => setUser(event.target.value, 'nickName')}
+        />
         <Button label="가입하기" />
       </FormContainer>
     </Container>
