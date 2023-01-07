7f51c9f0b4aae058258c56e29ca25c6509a31085
yongjunhyun <dev2diver@gmail.com>
2023-01-07 18:19:34 +0900

    feat: RegisterForm 컴포넌트 대신 UserRegisterPage 컴포넌트 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import { PageLayout } from '@/components/layout';
-import { RegisterForm } from '@/components/blocks';
+import { UserRegisterPage } from '@/pages';
 
 function App() {
   return (
     <PageLayout>
       <Routes>
-        <Route path="/" element={<RegisterForm />} />
+        <Route path="/" element={<UserRegisterPage />} />
       </Routes>
     </PageLayout>
   );
