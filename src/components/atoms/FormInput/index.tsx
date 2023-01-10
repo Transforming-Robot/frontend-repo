ee0addfe3c691d098d92c85a68683779d4739a9c
yongjunhyun <dev2diver@gmail.com>
2023-01-11 00:36:05 +0900

    feat: 회원가입 유효성 테스트 추가

--- src/components/atoms/FormInput/index.tsx
+++ src/components/atoms/FormInput/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 interface Props {
@@ -8,13 +8,23 @@ interface Props {
   label: string;
   placeHolder: string;
   value?: string;
+  validation?: boolean;
+  maxLength?: number;
+  required?: boolean;
   onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+interface StyledInputProps {
+  value?: string;
+  validation?: boolean;
 }
 
 const Container = styled.div`
   position: relative;
-  width: fit-content;
   height: fit-content;
+
+  display: flex;
+  flex-direction: column;
 `;
 
 const Label = styled.label`
@@ -32,7 +42,7 @@ const Label = styled.label`
   background-color: #ffffff;
 `;
 
-const StyledInput = styled.input`
+const StyledInput = styled.input<StyledInputProps>`
   font-family: 'Inter';
   font-style: normal;
   font-weight: 400;
@@ -44,30 +54,80 @@ const StyledInput = styled.input`
 
   padding: 12px 12px;
 
+  outline: none;
+
+  border-color: ${(props) => !props.validation && props.value !== '' && 'red'};
+  border-color: ${(props) =>
+    props.validation && props.value !== '' && '#00d300'};
+
   &::placeholder {
     color: #b1b8c0;
   }
 `;
 
-export const FormInput = ({
+const ValidationDescription = styled.div`
+  color: red;
+
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 14px;
+
+  padding: 8px;
+`;
+
+export function FormInput({
   id,
   className,
   type,
   label,
   placeHolder,
   value,
+  validation,
+  maxLength,
+  required,
   onChange,
-}: Props) => (
-  <Container>
-    <Label htmlFor={id}>{label}</Label>
-    <StyledInput
-      id={id}
-      className={className}
-      type={type}
-      placeholder={placeHolder}
-      value={value}
-      onChange={onChange}
-      spellCheck={false}
-    />
-  </Container>
-);
+}: Props) {
+  const [descriptionMessage, setDescriptionMessage] = useState('');
+
+  useEffect(() => {
+    switch (id) {
+      case 'email':
+        setDescriptionMessage('이메일 형식에 맞게 입력해주세요');
+        break;
+      case 'password':
+        setDescriptionMessage(
+          '비밀번호는 숫자 + 영문 + 특수문자 조합 8자리 이상이여야 합니다',
+        );
+        break;
+      case 'passwordCheck':
+        setDescriptionMessage('비밀번호가 일치하지 않습니다.');
+        break;
+      case 'nickName':
+        setDescriptionMessage('닉네임은 2자리 이상 5자리 이하여야 합니다');
+        break;
+    }
+  }, []);
+
+  return (
+    <Container>
+      <Label htmlFor={id}>{label}</Label>
+      <StyledInput
+        id={id}
+        className={className}
+        type={type}
+        placeholder={placeHolder}
+        value={value}
+        onChange={onChange}
+        spellCheck={false}
+        validation={validation}
+        maxLength={maxLength}
+        required={required}
+      />
+      {!validation && value !== '' ? (
+        <ValidationDescription>{descriptionMessage}</ValidationDescription>
+      ) : null}
+    </Container>
+  );
+}
--- src/components/blocks/RegisterForm/index.tsx
+++ src/components/blocks/RegisterForm/index.tsx
@@ -5,29 +5,46 @@ import axios from 'axios';
 import { FormInput, Button, HorizontalBar } from '@/components/atoms';
 import { useUserRegisterStore } from '@/stores';
 import { apiURL } from '@/common/apiURL';
+import { validationTest } from '@/utils';
 
 const FormContainer = styled.form`
   display: flex;
   flex-direction: column;
   gap: 20px;
 
-  width: fit-content;
+  width: 400px;
   height: fit-content;
 `;
 
 export function RegisterForm() {
-  const [user, setUser] = useUserRegisterStore((state) => [
+  const [user, setUser, setUserValidation] = useUserRegisterStore((state) => [
     state.user,
     state.setUser,
+    state.setUserValidation,
   ]);
 
   const onChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setUser(event.target.value, event.target.id);
+    const value = event.target.value;
+    const id = event.target.id;
+    const validationResult =
+      id === 'passwordCheck'
+        ? validationTest(id, value, user.password.value)
+        : validationTest(id, value);
+
+    setUser(value, id);
+    setUserValidation(validationResult, id);
   };
 
   const onClickHandler = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
-    axios.post(`${apiURL}/users/register`, user);
+    if (
+      user.email.validation &&
+      user.password.validation &&
+      user.passwordCheck.validation &&
+      user.nickName.validation
+    ) {
+      axios.post(`${apiURL}/users/register`, user);
+    } else return;
   };
 
   return (
@@ -38,31 +55,40 @@ export function RegisterForm() {
         id="email"
         label="이메일"
         placeHolder="이메일을 입력해주세요"
-        value={user.email}
+        value={user.email.value}
         onChange={onChangeHandler}
+        validation={user.email.validation}
+        required
       />
       <FormInput
         id="password"
         type="password"
         label="비밀번호"
         placeHolder="비밀번호를 입력해주세요"
-        value={user.password}
+        value={user.password.value}
         onChange={onChangeHandler}
+        validation={user.password.validation}
+        required
       />
       <FormInput
         id="passwordCheck"
         type="password"
         label="비밀번호 중복확인"
         placeHolder="비밀번호를 입력해주세요"
-        value={user.passwordCheck}
+        value={user.passwordCheck.value}
         onChange={onChangeHandler}
+        validation={user.passwordCheck.validation}
+        required
       />
       <FormInput
         id="nickName"
         label="닉네임"
         placeHolder="닉네임을 입력해주세요"
-        value={user.nickName}
+        value={user.nickName.value}
         onChange={onChangeHandler}
+        validation={user.nickName.validation}
+        maxLength={5}
+        required
       />
 
       <HorizontalBar />
