e380eb918043795d5ff1218f703dcca644b13c8d
yongjunhyun <dev2diver@gmail.com>
2023-01-11 13:39:13 +0900

    feat: useUserRegisterStore 리팩토링
    - 변수명 동적할당
    - if문은 switch case 문으로 변경

--- src/stores/UserRegister/index.ts
+++ src/stores/UserRegister/index.ts
@@ -3,10 +3,7 @@ import { devtools } from 'zustand/middleware';
 
 interface UserRegisterState {
   user: {
-    email: { value: string; validation: boolean };
-    password: { value: string; validation: boolean };
-    passwordCheck: { value: string; validation: boolean };
-    nickName: { value: string; validation: boolean };
+    [inputName: string]: { value: string; validation: boolean };
   };
   setUser: (value: string, id: string) => void;
   setUserValidation: (value: boolean, id: string) => void;
@@ -23,137 +20,101 @@ export const useUserRegisterStore = create<UserRegisterState>()(
       },
       setUser: (value, id) =>
         set((state) => {
-          if (id === 'email') {
-            return {
-              user: {
-                email: {
-                  value: value,
-                  validation: state.user.email.validation,
+          switch (id) {
+            case 'email':
+              return {
+                user: {
+                  ...state.user,
+                  email: {
+                    value: value,
+                    validation: state.user.email.validation,
+                  },
                 },
-                password: { ...state.user.password },
-                passwordCheck: { ...state.user.passwordCheck },
-                nickName: { ...state.user.nickName },
-              },
-            };
-          }
-
-          if (id === 'password') {
-            return {
-              user: {
-                email: { ...state.user.email },
-                password: {
-                  value: value,
-                  validation: state.user.password.validation,
+              };
+            case 'password':
+              return {
+                user: {
+                  ...state.user,
+                  password: {
+                    value: value,
+                    validation: state.user.password.validation,
+                  },
                 },
-                passwordCheck: { ...state.user.passwordCheck },
-                nickName: { ...state.user.nickName },
-              },
-            };
-          }
-
-          if (id === 'passwordCheck') {
-            return {
-              user: {
-                email: { ...state.user.email },
-                password: { ...state.user.password },
-                passwordCheck: {
-                  value: value,
-                  validation: state.user.passwordCheck.validation,
+              };
+            case 'passwordCheck':
+              return {
+                user: {
+                  ...state.user,
+                  passwordCheck: {
+                    value: value,
+                    validation: state.user.passwordCheck.validation,
+                  },
                 },
-                nickName: { ...state.user.nickName },
-              },
-            };
-          }
-
-          if (id === 'nickName') {
-            return {
-              user: {
-                email: { ...state.user.email },
-                password: { ...state.user.password },
-                passwordCheck: { ...state.user.passwordCheck },
-                nickName: {
-                  value: value,
-                  validation: state.user.nickName.validation,
+              };
+            case 'nickName':
+              return {
+                user: {
+                  ...state.user,
+                  nickName: {
+                    value: value,
+                    validation: state.user.nickName.validation,
+                  },
                 },
-              },
-            };
+              };
+            default:
+              return {
+                ...state.user,
+              };
           }
-
-          return {
-            user: {
-              email: { ...state.user.email },
-              password: { ...state.user.password },
-              passwordCheck: { ...state.user.passwordCheck },
-              nickName: { ...state.user.nickName },
-            },
-          };
         }),
       setUserValidation: (value, id) =>
         set((state) => {
-          if (id === 'email') {
-            return {
-              user: {
-                email: {
-                  value: state.user.email.value,
-                  validation: value,
+          switch (id) {
+            case 'email':
+              return {
+                user: {
+                  ...state.user,
+                  email: {
+                    value: state.user.email.value,
+                    validation: value,
+                  },
                 },
-                password: { ...state.user.password },
-                passwordCheck: { ...state.user.passwordCheck },
-                nickName: { ...state.user.nickName },
-              },
-            };
-          }
-
-          if (id === 'password') {
-            return {
-              user: {
-                email: { ...state.user.email },
-                password: {
-                  value: state.user.password.value,
-                  validation: value,
+              };
+            case 'password':
+              return {
+                user: {
+                  ...state.user,
+                  password: {
+                    value: state.user.password.value,
+                    validation: value,
+                  },
                 },
-                passwordCheck: { ...state.user.passwordCheck },
-                nickName: { ...state.user.nickName },
-              },
-            };
-          }
-
-          if (id === 'passwordCheck') {
-            return {
-              user: {
-                email: { ...state.user.email },
-                password: { ...state.user.password },
-                passwordCheck: {
-                  value: state.user.passwordCheck.value,
-                  validation: value,
+              };
+            case 'passwordCheck':
+              return {
+                user: {
+                  ...state.user,
+                  passwordCheck: {
+                    value: state.user.passwordCheck.value,
+                    validation: value,
+                  },
                 },
-                nickName: { ...state.user.nickName },
-              },
-            };
-          }
-
-          if (id === 'nickName') {
-            return {
-              user: {
-                email: { ...state.user.email },
-                password: { ...state.user.password },
-                passwordCheck: { ...state.user.passwordCheck },
-                nickName: {
-                  value: state.user.nickName.value,
-                  validation: value,
+              };
+            case 'nickName':
+              return {
+                user: {
+                  ...state.user,
+                  nickName: {
+                    value: state.user.nickName.value,
+                    validation: value,
+                  },
                 },
-              },
-            };
+              };
+            default:
+              return {
+                ...state.user,
+              };
           }
-
-          return {
-            user: {
-              email: { ...state.user.email },
-              password: { ...state.user.password },
-              passwordCheck: { ...state.user.passwordCheck },
-              nickName: { ...state.user.nickName },
-            },
-          };
         }),
     }),
     {
